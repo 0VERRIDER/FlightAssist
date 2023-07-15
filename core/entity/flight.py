d1555270bfd470c3a6a4a6d84a64c1ff6024c458
Anshil P <anshilp@karunya.edu.in>
2023-07-15 08:16:30 +0530

    Added: Flight_type

--- core/entity/flight.py
+++ core/entity/flight.py
@@ -1,12 +1,14 @@
 # class for flight entity
 class Flight:
     def __init__(
-            self, flight_id, 
+            self, 
+            flight_id, 
             flight_name, 
             flight_source, 
             flight_destination, 
             flight_fare, 
             flight_status,
+            flight_type,
             seat_arrangement,
             available_seats
         ):
@@ -17,8 +19,9 @@ class Flight:
         self.flight_destination = flight_destination
         self.flight_fare = flight_fare
         self.flight_status = flight_status
+        self.flight_type = flight_type
         self.seat_arrangement = seat_arrangement
         self.available_seats = available_seats
 
     def __str__(self):
-        return f'{self.flight_id} {self.flight_name} {self.flight_source} {self.flight_destination} {self.flight_fare} {self.flight_status} {self.seat_arrangement} {self.available_seats}'
+        return f'{self.flight_id} {self.flight_name} {self.flight_source} {self.flight_destination} {self.flight_fare} {self.flight_status} {self.flight_type} {self.seat_arrangement} {self.available_seats}'
